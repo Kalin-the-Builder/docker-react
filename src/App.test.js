9ec8a5fd9aa84ed39371f3e643b90bbb7b4f66d2
Kalin <kalin.pretorius20@gmail.com>
2021-04-25 21:02:09 +0200

    app changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the Demo!/i);
+  const linkElement = screen.getByText(/Learn React!/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the Demo!/i);
+  const linkElement = screen.getByText(/Learn React!/i);
   expect(linkElement).toBeInTheDocument();
 });
